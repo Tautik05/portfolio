4347356a5b0ea6d059b3c7d319c24e0057400250
Tautik05 <tautiksinharoy@gmail.com>
2025-02-03 00:05:08 +0530

    changed index title

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+
   <BrowserRouter>
+    <title>Tautik Sinha Roy</title>
     <div className="min-h-screen bg flex flex-col w-screen overscroll-none">
       <Navbar /> {/* Navbar remains constant */}
       <App /> {/* Routed content changes */}
